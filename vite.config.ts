e49fefca1c066ebfc5917c2329f9f0d859b40e76
FaisalShahi <shahifaisal132@gmail.com>
2026-04-21 18:39:25 +0530

    valid-icons-for-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -22,12 +22,12 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/pwa-192.png",
+            src: "/temp-icon.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/pwa-512.png",
+            src: "/temp-icon.png",
             sizes: "512x512",
             type: "image/png",
           },
